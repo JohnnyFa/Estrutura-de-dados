90bfd10a2d5d207ceefcc22c5d121a4f29b6e3c9
Johnny Fagundes <42876049+JohnnyFa@users.noreply.github.com>
2020-03-12 17:12:49 -0300

    Update Calculadora.java

--- socket/Calculadora.java
+++ socket/Calculadora.java
@@ -1,3 +1,7 @@
+//sendo produzido um "cliente servidor" onde o cliente vai mandar os comandos para calcular e o servidor 
+//fará o calculo e retornara na tela do cliente
+
+// só tem a parte de conexao no momento
 
 import java.net.*;
 import java.io.*;
