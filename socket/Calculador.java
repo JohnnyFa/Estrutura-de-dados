13f2cc1e881c09a6df7001011f2544ae1ebd2181
Johnny Fagundes <42876049+JohnnyFa@users.noreply.github.com>
2020-03-12 17:12:22 -0300

    Update Calculador.java

--- socket/Calculador.java
+++ socket/Calculador.java
@@ -1,6 +1,8 @@
 //sendo produzido um "cliente servidor" onde o cliente vai mandar os comandos para calcular e o servidor 
 //fará o calculo e retornara na tela do cliente
 
+// só tem a parte de conexao no momento
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
